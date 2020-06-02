f35a87e42e3b751e2715905fecf995bd4002b8c6
Pouyan Madanipour <pouyan.mad@gmail.com>
2020-06-02 14:47:24 +1000

    updates

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -28,13 +28,13 @@ export default function About() {
 					<div className='box2'>
 						<h2>Interests</h2>
 						<div className='text2'>
-							I design visual experiences to trasnfer ideas and concepts. I seek a creative approach to
+							I design visual experiences to trasnfer concepts and ideas. I seek a creative approach to
 							solve a problem that can result in simple yet innovative design solution.
 						</div>
 					</div>
 					<div className='box5'>
 						<h2>Stack</h2>
-						<div className='text5'>Javascript</div>
+						<div className='text5'>JavaScript</div>
 						<div className='text6'>React JS</div>
 						<div className='text5'>Node.js</div>
 						<div className='text6'>Express</div>
@@ -48,14 +48,14 @@ export default function About() {
 					<div className='box3'>
 						<h2>Vision</h2>
 						<div className='text3'>
-							Programming provides the opportunity to be more creative and productive. It provides an
-							imaginative mind a diverse possibilities to create diverse and innovative applications.
+							Programming provides an opportunity to be more creative and productive. It provides an
+							imaginative mind with diverse possibilities to create varied and innovative applications.
 						</div>
 					</div>
 					<div className='box4'>
 						<h2>Programming</h2>
 						<div className='text4'>
-							Coding is a tool to transfer ideas into a reality in order to solve big problems and make a
+							Coding is a tool to transfer ideas into a reality to solve big problems and make a
 							difference. I seek exposure to creative code as a programmer. I am passionate about clear
 							and well structured code.
 						</div>
