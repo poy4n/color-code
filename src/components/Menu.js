42e6285ee660a156b715802cbf0b5ba5a63216de
Pouyan Madanipour <pouyan.mad@gmail.com>
2020-06-27 15:51:19 +1000

    ga profile removed

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,70 +1,76 @@
-import React, { Component } from 'react';
-import { sample } from 'lodash';
+import React, { Component } from "react";
+import { sample } from "lodash";
 
-import './styles/Menu.css';
+import "./styles/Menu.css";
 
 export default class Mene extends Component {
-	state = {
-		isClicked: false,
-		colors: [ '#F39C12', '#F4D03F', '#AF7AC5', '#E74C3C', '#5DADE2', '#52BE80' ]
-	};
+  state = {
+    isClicked: false,
+    colors: ["#F39C12", "#F4D03F", "#AF7AC5", "#E74C3C", "#5DADE2", "#52BE80"],
+  };
 
-	handleClick = () => {
-		this.setState({
-			isClicked: !this.state.isClicked
-		});
-	};
+  handleClick = () => {
+    this.setState({
+      isClicked: !this.state.isClicked,
+    });
+  };
 
-	changeColor = () => {
-		return {
-			backgroundColor: sample(this.state.colors)
-		};
-	};
+  changeColor = () => {
+    return {
+      backgroundColor: sample(this.state.colors),
+    };
+  };
 
-	render() {
-		const links = [
-			{
-				label: 'ResumePDF',
-				url: 'https://drive.google.com/file/d/1le8x9UsGnLJRuBsbNYs3xKNRtFGi2gEu/view?usp=sharing'
-			},
-			{
-				label: 'LinkedIn',
-				url: 'https://www.linkedin.com/in/pouyanmadanipour/'
-			},
-			{
-				label: 'GitHub',
-				url: 'https://github.com/poy4n'
-			},
-			{
-				label: 'VisualWorks',
-				url: 'https://www.instagram.com/solitarius.digital/'
-			},
-			{
-				label: 'GAprofile',
-				url: 'https://profiles.generalassemb.ly/profiles/pouyanmadanipour'
-			}
-		];
-		const { isClicked } = this.state;
-		return (
-			<div className='menu-container'>
-				<div className='menu-button' onClick={this.handleClick}>
-					<i className='fas fa-bars' />
-				</div>
+  render() {
+    const links = [
+      {
+        label: "ResumePDF",
+        url:
+          "https://drive.google.com/file/d/1le8x9UsGnLJRuBsbNYs3xKNRtFGi2gEu/view?usp=sharing",
+      },
+      {
+        label: "LinkedIn",
+        url: "https://www.linkedin.com/in/pouyanmadanipour/",
+      },
+      {
+        label: "GitHub",
+        url: "https://github.com/poy4n",
+      },
+      {
+        label: "VisualWorks",
+        url: "https://www.instagram.com/solitarius.digital/",
+      },
+    ];
+    const { isClicked } = this.state;
+    return (
+      <div className="menu-container">
+        <div className="menu-button" onClick={this.handleClick}>
+          <i className="fas fa-bars" />
+        </div>
 
-				{links.length && (
-					<nav className={`menu ${isClicked ? 'show' : ''}`}>
-						<ul className='menu-items'>
-							{links.map((item) => (
-								<li style={this.changeColor(item.label)} className='menu-list' key={item.label}>
-									<a className='menu-link' href={item.url} target='_blank' rel='noopener noreferrer'>
-										{item.label}
-									</a>
-								</li>
-							))}
-						</ul>
-					</nav>
-				)}
-			</div>
-		);
-	}
+        {links.length && (
+          <nav className={`menu ${isClicked ? "show" : ""}`}>
+            <ul className="menu-items">
+              {links.map((item) => (
+                <li
+                  style={this.changeColor(item.label)}
+                  className="menu-list"
+                  key={item.label}
+                >
+                  <a
+                    className="menu-link"
+                    href={item.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        )}
+      </div>
+    );
+  }
 }
